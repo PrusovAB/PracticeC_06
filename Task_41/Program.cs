30ebfa7ec97d4561ba3925ed4190069a91ede17e
PrusovAB <Djromeo93@mail.ru>
2022-08-20 10:18:25 +0300

    add

---
+++ Task_41/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите числа через пробел: ");
+// Преобразуем полученное строчное значение в массив через пробел.
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+int SumPositiveNumber(int[] array)
+{
+   int sum = 0;
+   for (int i = 0; i < array.Length; i++)
+   {
+      if (array[i] > 0)
+      {
+         sum++;
+      }
+   }
+   return sum;
+}
+
+int sumPositiveNumber = SumPositiveNumber(arr);
+Console.WriteLine($"сумма положительный чисел введеных пользователем  = {sumPositiveNumber}");
